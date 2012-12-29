f137bc40258d908c8d314ce3f09a64a184fa3e79
David Edmundson <kde@davidedmundson.co.uk>
2012-12-29 12:13:51 +0000

    Add a model that allows for easy grouping of ContactsListModel wrapping all proxies up into a simple class
    Note this class is currently called ContactsModel2 because the ktp-common-internals model is not deleted yet, and not namespaced

---
+++ contacts-model.cpp
@@ -0,0 +1,53 @@
+#include "contacts-model.h"
+
+#include <KTp/Models/accounts-tree-proxy-model.h>
+#include <KTp/Models/groups-tree-proxy-model.h>
+
+ContactsModel2::ContactsModel2(QObject *parent)
+    : ContactsFilterModel(parent),
+      m_source(new KTp::ContactsListModel(this)),
+      m_groupMode(NoGrouping)
+{
+
+}
+
+void ContactsModel2::setAccountManager(const Tp::AccountManagerPtr &accountManager)
+{
+    m_accountManager = accountManager;
+
+    setGroupMode(m_groupMode); //reload the groups, as AccountGrouping now needs a real accountManager
+    m_source->setAccountManager(accountManager);
+}
+
+void ContactsModel2::setGroupMode(ContactsModel2::GroupMode mode)
+{
+
+    m_groupMode = mode;
+
+    if (!m_accountManager) {
+        mode = NoGrouping; //don't waste time doing any grouping if account manager is not really ready.. as we will have nothing to group anyway
+    }
+
+    if (m_proxy) {
+        m_proxy.data()->deleteLater();
+    }
+
+    switch (mode) {
+    case NoGrouping:
+        setSourceModel(m_source);
+        break;
+    case AccountGrouping:
+        m_proxy = new AccountsTreeProxyModel(m_source, m_accountManager);
+        setSourceModel(m_proxy.data());
+        break;
+    case GroupGrouping:
+        m_proxy = new GroupsTreeProxyModel(m_source);
+        setSourceModel(m_proxy.data());
+        break;
+    }
+}
+
+ContactsModel2::GroupMode ContactsModel2::groupMode() const
+{
+    return m_groupMode;
+}
